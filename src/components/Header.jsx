03f9e9db814a21c176bffe92c739853d86f16166
carter-ktb21 <144170194+carter-ktb21@users.noreply.github.com>
2024-05-22 18:16:43 -0400

    CSS Edits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,37 +10,37 @@ const Header = () => {
                 style={{
                     display: "flex", flexDirection: "column", alignItems: "flex-start", backgroundColor: "rgb(2,0,36)",
                     background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(119,35,184,1) 0%, rgba(0,0,0,1) 100%)", width: "100%"
-                }}>
-                <div
-                    style={{
-                        display: "flex", flexDirection: "column", alignItems: "center", padding: "1%"
-                    }}>
-                    {/* If user is logged in, logo button goes to home page. Otherwise, it goes to landing page */}
-                    <Link to={isLoggedIn ? "/homePage" : "/"}>
-                        <img
-                            style={{
-                                maxHeight: "80px", maxWidth: "80px"
-                            }} src="/assets/Anvil.webp" alt="AI Character Forge Logo" />
-                    </Link>
-                    <h1
+                }}
+            >
+                <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                    <div
                         style={{
-                            fontSize: "13px"
+                            display: "flex", flexDirection: "column", alignItems: "center", padding: "2%", height: "100%", marginRight: "2%"
                         }}>
-                        AI Character Forge
-                    </h1>
+                        {/* If user is logged in, logo button goes to home page. Otherwise, it goes to landing page */}
+                        <Link to={isLoggedIn ? "/homePage" : "/"}>
+                            <img
+                                className="header-logo"
+                                src="/assets/Anvil.webp" alt="AI Character Forge Logo"
+                            />
+                        </Link>
+                        <h1
+                            className="header-font"
+                        >
+                            AI Character Forge
+                        </h1>
+                    </div>
+                    {isLoggedIn && (
+                        <div
+                            className="user-profile"
+                        >
+                            <p>User: {userEmail}</p>
+                            <button onClick={logout}>Logout</button>
+
+                        </div>
+                    )}
                 </div>
             </div>
-            {isLoggedIn && (
-                <div
-                    style={{
-                        display: "flex", flexDirection: "column", alignItems: "center", position: "absolute", padding: "2%", marginLeft: "7.5%"
-                    }}
-                >
-                    <p>User: {userEmail}</p>
-                    <button onClick={logout}>Logout</button>
-                    
-                </div>
-            )}
         </div>
     );
 }
